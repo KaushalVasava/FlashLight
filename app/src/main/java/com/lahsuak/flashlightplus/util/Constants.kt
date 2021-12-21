9eb5a416f14e02f8d812ed5f6efa8421dcbb9ef1
KauhsalVasava <vasavakaushal333@gmail.com>
2021-12-21 12:48:54 +0530

    Version 1.0.0

---
+++ app/src/main/java/com/lahsuak/flashlightplus/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.lahsuak.flashlightplus.util
+
+internal const val SETTING_DATA = "SETTING_DATA"
+internal const val HAPTIC_FEEDBACK = "haptic_feedback"
+internal const val TOUCH_SOUND = "sound_feedback"
+internal const val FLASH_ON_START = "flash_on_start"
+internal const val BIG_FLASH_AS_SWITCH = "big_flash_as_switch"
+internal const val SHAKE_TO_LIGHT = "shake_to_light"
+internal const val SOS_NUMBER = "sos_number"
+internal const val PHONE_REQUEST_CODE = 102
+internal const val READ_PHONE_STATE_REQUEST_CODE = 101
+internal const val CAMERA_REQUEST_CODE = 103
+internal const val PHONE_STATE_RECEIVED = "phone_state_received"
+internal const val SHAKE_SENSITIVITY = "shake_sensitivity"
